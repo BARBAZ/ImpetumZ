e0180435e5598e529eba40efb541ff7f845eb131
barbaz <kevin_barbazan@live.fr>
2020-11-13 16:02:50 +0100

    File Format Parsing OK

--- ImpetumZ.py
+++ ImpetumZ.py
@@ -1,6 +1,18 @@
 
 ###################################################################### Preprocessing ######################################################################
 
+###### Python Builtin modules #####
+
+import io
+import os
+import struct
+import sys
+
+##### Autodesk Maya's API Modules #####
+
+import maya.cmds as cmds
+import maya.api.OpenMaya as om
+
 ###########################################################################################################################################################
 
 ############################################################## Variables / Arrays Definitions ##############################################################
@@ -37,6 +49,11 @@
 
 
 # 3D #
+
+
+# Anim #
+
+
          
 ##### Pythonic Functions #####
 
@@ -45,7 +62,6 @@
 def Log_Arrays(array):
     print ""
     for i in range(len(array)):
-        
         element = array[i]
         print element
     print ""
@@ -70,7 +86,7 @@ def Clean_Name(text):
     text4 = "".join(Strings)
     return text4
 
-# Misc #
+# ImpetumZ #
 
 def Open_File():
     fullpath = cmds.fileDialog2(fileMode=1)
@@ -81,24 +97,83 @@ def Open_File():
     realpath = fullpath.replace(filename,"")
     os.chdir(realpath)
     file_object = io.open(filename,'r+b')
-    Import_File(file_object)
+    return file_object
 
-def Main_Loop(run_count, loop):
-    Open_File()    
-
-def Import_File(file_object):
+def File_Header(file_object):
     xsm = file_object
-    XSM_Magic = struct.unpack('<4p', xsm.read(4))
-    print XSM_Magic
+    print xsm.tell()
+    xsm.seek(16, os.SEEK_CUR)
+    return xsm
+
+def File_MetaData(file_object):
+    xsm = file_object
+    print xsm.tell()
+    xsm.seek(20, os.SEEK_CUR)
+    SoftName_Length = struct.unpack('<I', xsm.read(4))[0]
+    print SoftName_Length
+    SoftName = xsm.read(SoftName_Length)
+    SoftPath_Length = struct.unpack('<I', xsm.read(4))[0]
+    print SoftPath_Length
+    SoftPath = xsm.read(SoftPath_Length)
+    CompileDate_Length = struct.unpack('<I', xsm.read(4))[0]
+    print CompileDate_Length
+    CompileDate = xsm.read(CompileDate_Length)
+    xsm.seek(4, os.SEEK_CUR)
+    xsm.seek(12, os.SEEK_CUR)
+    Object_Count = struct.unpack('<I', xsm.read(4))[0]
+    return xsm, Object_Count #, SoftName, SoftPath, CompileDate
+
+def FileData(file_object):
+    xsm = file_object
+    print xsm.tell()
+    xsm.seek(16, os.SEEK_CUR)
+    xsm.seek(16, os.SEEK_CUR)
+    xsm.seek(24, os.SEEK_CUR)
+    xsm.seek(24, os.SEEK_CUR)
+    Type0_Count = struct.unpack('<I', xsm.read(4))[0]
+    Type1_Count = struct.unpack('<I', xsm.read(4))[0]
+    Type2_Count = struct.unpack('<I', xsm.read(4))[0]
+    Type3_Count = struct.unpack('<I', xsm.read(4))[0]
+    if(Type0_Count > 0):
+        print "Type 0 anim"
+    if(Type1_Count > 0):
+        print "Type 1 anim"
+    if(Type2_Count > 0):
+        print "Type 2 anim"
+    if(Type3_Count > 0):
+        print "Type 3 anim"
+    xsm.seek(4, os.SEEK_CUR)
+    BoneName_Length = struct.unpack('<I', xsm.read(4))[0]
+    print xsm.tell()
+    BoneName = xsm.read(BoneName_Length)
+    print BoneName
+    for i in range(Type0_Count):
+        xsm.seek(16, os.SEEK_CUR)
+    for i in range(Type1_Count):
+        xsm.seek(12, os.SEEK_CUR)
+    for i in range(Type2_Count):
+        xsm.seek(16, os.SEEK_CUR)
+    for i in range(Type3_Count):
+        xsm.seek(12, os.SEEK_CUR)
+    
+    return xsm  #, KeyFrames_Count, BonesName
+    
  
 ###########################################################################################################################################################
 
 ################################################################### Script instructions ###################################################################
 
-Open_File()
+Args = File_MetaData(File_Header(Open_File()))
+print Args
+xsm = Args[0]
 
+for i in range(Args[1]):
+    FileData(xsm)
+
+print xsm.tell()
+print "EOF"
+xsm.close()
 print "\n"
 print "EOS"
-
 
 ###########################################################################################################################################################
