080542e5af69a9d7dc0b6b8e87457e967ad3d4e5
barbaz <kevin_barbazan@live.fr>
2020-11-17 20:37:56 +0100

    Dumped Keyframes Data to Dictionnary

--- ImpetumZ.py
+++ ImpetumZ.py
@@ -27,7 +27,7 @@ import maya.api.OpenMaya as om
 
 ##### Dictionnaries #####
 
-
+Animation = {}
 
 ##### Constants #####
 
@@ -45,16 +45,35 @@ import maya.api.OpenMaya as om
 
 ##### Maya Functions #####
 
-# UI #
+# ImpetumZ #
 
+def keyframe12(): 
+    Byte_0 = xsm.read(1)
+    Byte_1 = xsm.read(1)
+    Bytes0 = struct.pack('2c0l', Byte_0, Byte_1)
+    Quat0 = struct.unpack('<f', Bytes0)
+    Byte_2 = xsm.read(1)
+    Byte_3 = xsm.read(1)
+    Bytes1 = struct.pack('2c0l', Byte_2, Byte_3)
+    Quat1 = struct.unpack('<f', Bytes1)
+    Byte_4 = xsm.read(1)
+    Byte_5 = xsm.read(1)
+    Bytes2 = struct.pack('2c0l', Byte_4, Byte_5)
+    Quat2 = struct.unpack('<f', Bytes2)  
+    Byte_6 = xsm.read(1)
+    Byte_7 = xsm.read(1)
+    Bytes3 = struct.pack('2c0l', Byte_6, Byte_7)
+    Quat3 = struct.unpack('<f', Bytes3)
+    Time = struct.unpack('<f', xsm.read(4))
+    return Quat0, Quat1, Quat2, Quat3, Time
 
-# 3D #
-
-
-# Anim #
-
-
-         
+def keyframe16():
+    Float0 = struct.unpack('<f', xsm.read(4))
+    Float1 = struct.unpack('<f', xsm.read(4))
+    Float2 = struct.unpack('<f', xsm.read(4))
+    Float3 = struct.unpack('<f', xsm.read(4))
+    return Float0, Float1, Float2, Float3
+    
 ##### Pythonic Functions #####
 
 # Array Operations #
@@ -65,15 +84,6 @@ def Log_Arrays(array):
         element = array[i]
         print element
     print ""
-
-def create_list(itm_cnt, list, num_element): # Itm_cnt = numbers of items in list - list = an initialized python list item - Num_element = number of element per row 
-    index = 0
-    for i in range(itm_cnt):
-        list.append([])
-        for j in range(num_element):
-            list[index].append([])
-        index += 1
-    return list
 
 # Strings Operations #
 
@@ -86,7 +96,7 @@ def Clean_Name(text):
     text4 = "".join(Strings)
     return text4
 
-# ImpetumZ #
+# IO
 
 def Open_File():
     fullpath = cmds.fileDialog2(fileMode=1)
@@ -99,33 +109,28 @@ def Open_File():
     file_object = io.open(filename,'r+b')
     return file_object
 
+# ImpetumZ #
+
 def File_Header(file_object):
     xsm = file_object
-    print xsm.tell()
     xsm.seek(16, os.SEEK_CUR)
     return xsm
 
 def File_MetaData(file_object):
     xsm = file_object
-    print xsm.tell()
     xsm.seek(20, os.SEEK_CUR)
     SoftName_Length = struct.unpack('<I', xsm.read(4))[0]
-    print SoftName_Length
     SoftName = xsm.read(SoftName_Length)
     SoftPath_Length = struct.unpack('<I', xsm.read(4))[0]
-    print SoftPath_Length
     SoftPath = xsm.read(SoftPath_Length)
     CompileDate_Length = struct.unpack('<I', xsm.read(4))[0]
-    print CompileDate_Length
     CompileDate = xsm.read(CompileDate_Length)
     xsm.seek(4, os.SEEK_CUR)
     xsm.seek(12, os.SEEK_CUR)
     Object_Count = struct.unpack('<I', xsm.read(4))[0]
     return xsm, Object_Count #, SoftName, SoftPath, CompileDate
 
-def FileData(file_object):
-    xsm = file_object
-    print xsm.tell()
+def FileData():
     xsm.seek(16, os.SEEK_CUR)
     xsm.seek(16, os.SEEK_CUR)
     xsm.seek(24, os.SEEK_CUR)
@@ -134,43 +139,48 @@ def FileData(file_object):
     Type1_Count = struct.unpack('<I', xsm.read(4))[0]
     Type2_Count = struct.unpack('<I', xsm.read(4))[0]
     Type3_Count = struct.unpack('<I', xsm.read(4))[0]
-    if(Type0_Count > 0):
-        print "Type 0 anim"
-    if(Type1_Count > 0):
-        print "Type 1 anim"
-    if(Type2_Count > 0):
-        print "Type 2 anim"
-    if(Type3_Count > 0):
-        print "Type 3 anim"
-    xsm.seek(4, os.SEEK_CUR)
+    xsm.seek(4, os.SEEK_CUR) # C++ compiler delimiter 0xFFFF7F7F
     BoneName_Length = struct.unpack('<I', xsm.read(4))[0]
-    print xsm.tell()
     BoneName = xsm.read(BoneName_Length)
-    print BoneName
+    print Clean_Name(BoneName)
+    Keyframes0 = []
+    Keyframes1 = []
+    Keyframes2 = []
+    Keyframes3 = []
+
     for i in range(Type0_Count):
-        xsm.seek(16, os.SEEK_CUR)
+        Keyframes0.append([])
+        Keyframes0[i].append(keyframe16())
     for i in range(Type1_Count):
-        xsm.seek(12, os.SEEK_CUR)
+        Keyframes1.append([])
+        Keyframes1[i].append(keyframe12())
     for i in range(Type2_Count):
-        xsm.seek(16, os.SEEK_CUR)
+        Keyframes2.append([])
+        Keyframes2[i].append(keyframe16())
     for i in range(Type3_Count):
-        xsm.seek(12, os.SEEK_CUR)
-    
-    return xsm  #, KeyFrames_Count, BonesName
-    
- 
+        Keyframes3.append([])
+        Keyframes3[i].append(keyframe12())
+
+    Log_Arrays(Keyframes0)
+    Log_Arrays(Keyframes1)
+    Log_Arrays(Keyframes2)
+    Log_Arrays(Keyframes3)
+    return Keyframes0, Keyframes1, Keyframes2, Keyframes3, BoneName
+
+
 ###########################################################################################################################################################
 
 ################################################################### Script instructions ###################################################################
 
 Args = File_MetaData(File_Header(Open_File()))
-print Args
 xsm = Args[0]
 
 for i in range(Args[1]):
-    FileData(xsm)
-
-print xsm.tell()
+    Args0 = FileData()
+    for i in range(3):
+        if(len(Args0[i]) > 0):
+            Animation[Args0[4]] = Args0[i]
+  
 print "EOF"
 xsm.close()
 print "\n"
